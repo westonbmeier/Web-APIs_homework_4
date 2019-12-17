4d42a01aebdddca5f583187d64e92b937c6833c7
westonbmeier <westonbmeier@gmail.com>
2019-12-17 16:14:28 -0700

    Score complete. Need to finish localstorage

--- Web-APIs_homework_4/README.md
+++ Web-APIs_homework_4/README.md
@@ -1,2 +1,21 @@
 ## Web-APIs homework number 4
-This is a timed quiz testing your knowledge of a small part of the culinary world. you will have 75 seconds to answer 5 questions. Google is allowed but you have you be quick. If a question is answered incorrectly, there will be a five second time penalty and you will move on to the next question. The score will be tallied at the end and saved to local storage. 
+This is a timed quiz testing your knowledge of a small part of the culinary world. you will have 75 seconds to answer 5 questions. Google is allowed but you have you be quick. If a question is answered incorrectly, there will be a five second time penalty and you will move on to the next question. The score will be whatever the time limit is at the end of the quiz. 
+
+## Javascript Used
+The Javascript used in this application is primarily for populating the question and answer boxes, as well as keeping time and submitting the users name and scores. The users and scores are saved on local storage in the DOM. 
+
+## Styling 
+The styling is simple and straight forward. 
+
+background-color: ghostwhite;
+font-color: #042d3a
+font-family: 'Times New Roman', Times, serif;
+Box-shadow: #80defa;
+All content is centered. 
+
+## Screenshots 
+![Index Page](resources/index.PNG)
+![Quiz](resources/quiz.PNG)
+![Submit](resources/submit.PNG)
+
+
--- Web-APIs_homework_4/questions.js
+++ Web-APIs_homework_4/questions.js
@@ -12,7 +12,7 @@ var questions = [
     {
       question: "Mario Batali was a celebrity chef known for his show:",
       choices: ["Emril Live", "Barefoot Contessa", "Tool Time", "Molto Mario"],
-      answer: "alerts"
+      answer: "Molto Mario"
     },
     {
       question: "The key to making french onion soup is:",
@@ -20,9 +20,9 @@ var questions = [
       answer: "All of the above"
      },
     {
-      question: "New Orleans cuisine is mostly influenced by what country/:",
+      question: "New Orleans cuisine is mostly influenced by what country:",
       choices: ["Germany", "Italy", "Canada", "France"],
-      answer: "alerts"
+      answer: "France"
     },
       
     
@@ -30,57 +30,109 @@ var questions = [
   
 
   const question = document.getElementById('question');
-  const choices = Array.from(document.getElementsByClassName('choice-text')); 
-  
-  let currentQuestion = {};
-  let acceptingAnswers = false; 
-  let score = 0; 
+  const choice1 = document.getElementById('choice1');
+  const choice2 = document.getElementById('choice2');
+  const choice3 = document.getElementById('choice3'); 
+  const choice4 = document.getElementById('choice4');
+  const quizContainer = document.getElementById('quizContainer');
+  const resultAlert = document.getElementById('resultAlert');
+  const timer = document.getElementById('timer')
   let questionCounter = 0; 
-  let availableQuestions = []; 
+  let questionTime = 75; 
+  var quizOver = false;
+  var finalScore = 0; 
   
-  
-  console.log(choices)
-  console.log(questions)
   startGame = () => {
-      questionCounter = 0
+      questionCounter = 0;
       score = 0; 
-      availableQuestions = []; 
-      getNewQuestion();
+      generateQuestions();
+      bestTimer();
   }; 
+
+  function bestTimer(){
+    var time = setInterval(function(){
+      questionTime--; 
+      timer.innerText = "Time:" + questionTime; 
+      if (questionTime <= 0){
+        clearInterval(time); 
+      } if (quizOver === true){
+        clearInterval(time); 
+      }
+    },1000);
+  }
+
+  generateQuestions = () => { 
+    if (questionCounter === 5) {
+      quizOver === true; 
+      endGame();
+    } else {
+    question.innerText = questions[questionCounter].question; 
+    choice1.innerText = questions[questionCounter].choices[0]; 
+    choice2.innerText = questions[questionCounter].choices[1]; 
+    choice3.innerText = questions[questionCounter].choices[2]; 
+    choice4.innerText = questions[questionCounter].choices[3]; 
+    }
+  }
+
+  quizContainer.addEventListener("click", function (e) {
+    if (e.target.classList.contains('choices')){
+      if (e.target.innerText === questions[questionCounter].answer){
+        var correctAlert = document.createElement("div");
+        correctAlert.classList.add("correctAnswer");
+        correctAlert.innerText = "Correct!";
+        resultAlert.appendChild(correctAlert); 
+        setTimeout(function(){
+          questionCounter++;
+          resultAlert.removeChild(correctAlert); 
+          generateQuestions();
+        }, 500)
+      }
+    
+      else {
+        var falseAlert = document.createElement("div");
+        falseAlert.innerText = "False!";
+        falseAlert.classList.add("correctAnswer");
+        resultAlert.appendChild(falseAlert); 
+        setTimeout(function(){
+          questionCounter++;
+          questionTime -= 10;
+          resultAlert.removeChild(falseAlert); 
+          generateQuestions();
+        }, 500)
+      }
+    }
+
+  }); 
+
+
+
+  function endGame() {  
+    quizOver = true; 
+    quizContainer.innerText = ""; 
+    var endGameAlert = document.createElement("div"); 
+    endGameAlert.innerText = "Complete! Your Score is: " + questionTime + "!"; 
+    endGameAlert.classList.add("complete"); 
+    var nameInput = document.createElement("input");
+    nameInput.type = "text";
+    nameInput.classList.add("name");
+    nameInput.placeholder = "Name"; 
+    var submit = document.createElement("INPUT");
+    submit.setAttribute("type", "submit");
+    submit.classList.add("submit-button");
+    quizContainer.appendChild(endGameAlert);
+    quizContainer.appendChild(nameInput);  
+    quizContainer.appendChild(submit); 
+   }
+
+
   
-  
-  getNewQuestion = () => {
-      questionCounter++; 
-      const questionIndex = Math.floor(Math.random(availableQuestions.length));
-      currentQuestion = availableQuestions[questionIndex]; 
-      question.innerText = currentQuestion.question; 
-      
-      choice.forEach(choice =>{
-          const number = choice.dataset['number']; 
-          choice.innerText = currentQuestion['choice' + number];
-      });
-      
-      availableQuestions.splice(questionIndex, 1); 
-  
-      acceptingAnswers = true; 
-  }; 
-  
-      choices.forEach(choice => {
-          choice.addEventListener('click', e =>{
-              if (!acceptingAnswers) return;
-              
-              acceptingAnswers = false;
-              const selectedChoice = e.target; 
-              const selectedAnswer = selectedChoice.dataset['number']; 
-          })
-           
-  
-      }); 
-  
-      
-  
-  
-  
-  
-  startGame(); 
-  
+
+
+
+  startGame();
+
+
+
+
+
+
